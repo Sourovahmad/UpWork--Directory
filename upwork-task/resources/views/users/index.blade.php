070d2cda8247403d884cee63d6a54e7abc88bd8d
sourovahmad <mdsourovahmad19@gmail.com>
2022-08-14 09:01:05 +0600

    layout removed

--- upwork-task/resources/views/users/index.blade.php
+++ upwork-task/resources/views/users/index.blade.php
@@ -22,33 +22,40 @@
 
     <!-------------------------------NAvigation Starts------------------>
 
-    <nav class="navbar navbar-expand-md navbar-dark mb-4" style="background-color:#3097D1">
-        <a href="{{ route('home') }}" class="navbar-brand"><img src="{{ asset('images/'.auth()->user()->image_file) }}" alt="logo" class="img-fluid" width="65px" height="55px" style="border-radius: 53px"></a>
 
-        <button class="navbar-toggler" data-toggle="collapse" data-target="#responsive"><span class="navbar-toggler-icon"></span></button>
+    
 
 
-        <div class="collapse navbar-collapse" id="responsive">
-            <ul class="navbar-nav mr-auto text-capitalize">
-                <li class="nav-item"><a href="index.html" class="nav-link active">home</a></li>
-                {{--
-                <li class="nav-item"><a href="profile.html" class="nav-link">profile</a></li>
-                <li class="nav-item"><a href="#modalview" class="nav-link" data-toggle="modal">messages</a></li>
-                <li class="nav-item"><a href="notification.html" class="nav-link">docs</a></li>
-                <li class="nav-item"><a href="#" class="nav-link d-md-none">growl</a></li>
-                <li class="nav-item"><a href="#" class="nav-link d-md-none">logout</a></li> --}}
+<nav class="mb-4 navbar navbar-expand-lg navbar-dark bg-dark cyan">
+    <a class="navbar-brand font-bold text-light">Your Website Title</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent-4" aria-controls="navbarSupportedContent-4" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSuapportedContent-4">
+        <ul class="navbar-nav ml-auto">
 
-            </ul>
+            <li class="nav-item dropdown" style="cursor: pointer">
+                <a class="nav-link dropdown-toggle text-light" id="navbarDropdownMenuLink-4" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fa fa-user text-light"></i> Profile </a>
+                <div class="dropdown-menu dropdown-menu-right dropdown-cyan" aria-labelledby="navbarDropdownMenuLink-4">
+                    <a class="dropdown-item" href="#">Change Password</a>
+                    <a class="dropdown-item" href="{{ route('logout') }}"   onclick="event.preventDefault();
+                    document.getElementById('logout-form').submit();">Log out</a>
+                    
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        @csrf
+                    </form>
+                </div>
+            </li>
+        </ul>
+    </div>
+</nav>
 
-            <form action="" class="form-inline ml-auto d-none d-md-block">
-                <input type="text" name="search" id="search" placeholder="Search" class="form-control form-control-sm">
-            </form>
-            <a href="{{ route('home') }}" class="text-decoration-none" style="color:#CBE4F2;font-size:22px;"><i class="far fa-bell ml-3 d-none d-md-block"></i></a>
-            <img src="{{ asset('images/'.auth()->user()->image_file) }}" alt="" class="rounded-circle ml-3 d-none d-md-block" width="32px" height="32px">
 
-        </div>
 
-    </nav>
+
+
+
+
 
     <!---------------------------------------------Ends navigation------------------------------>
 
@@ -65,73 +72,8 @@
 
             <!--------------------------left columns  start-->
 
-            <div class="col-12 col-lg-3">
-
-                <div class="left-column">
-
-
-                    <div class="card card-left1 mb-4">
-                        <div class="card-body text-center ">
-                            <img src="{{ asset('images/'.auth()->user()->image_file) }}" alt="img" width="120px" height="120px" class="rounded-circle mt-n5">
-                            <h5 class="card-title">{{ auth()->user()->name }}</h5>
-                            <p class="card-text text-justify mb-2">{{ auth()->user()->address }}</p>
-
-
-                        </div>
-
-
-
-
-                    </div>
-
-
-                    <div class="card shadow-sm card-left2 mb-4">
-
-                        <div class="card-body">
-
-                            <h5 class="mb-3 card-title">About </h5>
-
-                            {{-- <p class="card-text"> <i class="fas fa-calendar-week mr-2"></i> Went to  <a href="#" class="text-decoration-none">{{ auth()->user()->address }} </a></p> --}}
-
-                            <p class="card-text"> <i class="far fa-building mr-2"></i> Work at <a href="#" class="text-decoration-none">{{ auth()->user()->bussiness_and_company_name }}</a></p>
-                            <p class="card-text"> <i class="fas fa-home mr-2"></i> Live in <a href="#" class="text-decoration-none">{{ auth()->user()->address }}</a></p>
-                            <p class="card-text"> <i class="fas fa-map-marker mr-2"></i> Designation <a href="#" class="text-decoration-none">{{ auth()->user()->designation }}</a></p>
-                            <p class="card-text"> <i class="fas fa-map-marker mr-2"></i> Gender <a href="#" class="text-decoration-none">{{ auth()->user()->gender }}</a></p>
-                            <p class="card-text"> <i class="fas fa-map-marker mr-2"></i> Marital Status <a href="#" class="text-decoration-none">{{ auth()->user()->marital_status }}</a></p>
-
-
-
-
-                        </div>
-
-
-                        <div class="text-center mb-3">
-                            <a href="{{ route('logout') }}"
-                            onclick="event.preventDefault();
-                            document.getElementById('logout-form').submit();" class="btn btn-outline-info card-link btn-sm">Logout</a>
-
-
-                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                        @csrf
-                    </form>
-                        </div>
-
-                    </div>
-
-
-
-                </div>
-
-
-
-
-
-            </div>
-
-
-
-
-
+           
+            <div class="col-1"></div>
 
 
 
@@ -146,7 +88,7 @@
 
 
 
-            <div class="col-12 col-lg-6" style="height: 100vh; overflow-y:scroll;">
+            <div class="col-12 col-lg-8" style="height: 100vh; overflow-y:scroll;">
 
 
                 <div class="middle-column">
